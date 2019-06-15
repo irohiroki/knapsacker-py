efdc3f30048e9cf5d6924889a33348a18d0fdb99
Hiroki Yoshioka <hiroki.yoshioka@drecom.co.jp>
2019-06-15 17:58:04 +0900

    Tests

--- runner.py
+++ runner.py
@@ -4,6 +4,25 @@ class Item:
     self.value = value
     self.cost = cost
 
-items = [Item(1, 4), Item(3, 2)]
-for i in Knapsacker(items, capacity=4).pack():
-  print(i.value)
+items = [
+ # test 1: capacity 20
+  # Item(3, 7),
+  # Item(5, 3),
+  # Item(4, 8),
+  # Item(9, 3),
+  # Item(10, 9),
+  # Item(6, 11)
+ # test 2: capacity 60
+  # Item(30, 5),
+  # Item(20, 10),
+  # Item(100, 20),
+  # Item(90, 30),
+  # Item(160, 40)
+ # test 3: capacity 15
+  Item(10, 2),
+  Item(10, 4),
+  Item(12, 6),
+  Item(18, 9)
+]
+for i in Knapsacker(items, capacity=15).pack():
+  print(str(i.value) + "-" + str(i.cost))
